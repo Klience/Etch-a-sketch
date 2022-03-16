c419ce9db825b71b4fc9a5bcc883cd4e2fcc42db
Klience <94982318+Klience@users.noreply.github.com>
2022-03-16 20:56:05 +0000

    Add functionality, rename functions and vars, add comments.

--- script.js
+++ script.js
@@ -15,35 +15,38 @@ const rainbowBtn = document.querySelector('#rainbowBtn');
 const eraseBtn = document.querySelector('#eraseBtn');
 const clearBtn = document.querySelector('#clearBtn');
 
-sizePicker.addEventListener('mousemove', e => getSize(e.target.value));
-sizePicker.addEventListener('change', e => setSize(e.target.value));
-colorPicker.addEventListener('change', e => getColor(e.target.value));
-colorBtn.addEventListener('click', () => setMode('color'));
-rainbowBtn.addEventListener('click', () => setMode('rainbow'));
-eraseBtn.addEventListener('click', () => setMode('erase'));
+sizePicker.addEventListener('mousemove', e => setSizeValue(e.target.value));
+sizePicker.addEventListener('change', e => setNewSize(e.target.value));
+colorPicker.addEventListener('change', e => setNewColor(e.target.value));
+colorBtn.addEventListener('click', () => setCurrentMode('color'));
+rainbowBtn.addEventListener('click', () => setCurrentMode('rainbow'));
+eraseBtn.addEventListener('click', () => setCurrentMode('erase'));
 clearBtn.addEventListener('click', () => resetGrid());
 
-console.log(currentColor);
-
-function getSize(size) {
+// Updates grid size text: "size x size"
+function setSizeValue(size) {
   sizeValue.innerText = `${size} x ${size}`;
 }
 
-function getColor(color) {
-  return currentColor = color;
+// Updates current color with new color from color picker
+function setNewColor(newColor) {
+  currentColor = newColor;
 }
 
-function setSize(size) {
-  currentSize = size;
-  console.log(currentSize);
+// Updates current size with new size from size picker
+function setNewSize(newSize) {
+  currentSize = newSize;
+  drawGrid(newSize);
 }
 
-function setMode(mode) {
-  setActiveBtn(mode);
-  currentMode = mode;
+// Updates current mode with new mode from a corresponding button
+function setCurrentMode(newMode) {
+  setActiveBtn(newMode);
+  currentMode = newMode;
 }
 
-function setActiveBtn(mode) {
+// Updates current mode's button style as active
+function setActiveBtn(newMode) {
   if (currentMode === 'rainbow') {
     rainbowBtn.classList.remove('active');
   } else if (currentMode === 'color') {
@@ -52,42 +55,22 @@ function setActiveBtn(mode) {
     eraseBtn.classList.remove('active');
   }
 
-  if (mode === 'rainbow') {
+  if (newMode === 'rainbow') {
     rainbowBtn.classList.add('active');
-  } else if (mode === 'color') {
+  } else if (newMode === 'color') {
     colorBtn.classList.add('active');
-  } else if (mode === 'erase') {
+  } else if (newMode === 'erase') {
     eraseBtn.classList.add('active');
   }
 }
 
-
-/*
-function setMode() {
-  // color -- color I picked
-  // rainbow -- random color each time
-  // erase -- revert to default color
-
-  // this function returns color mode only (not value)?
-}
-function getMode() {
-  // GET AND SET SHALL BE INTERCHANGED
-}
-function setColor() {
-  // returns color I picked or random color, or default (erase)
-  // set via 'node.style.color.value'
-  // put erase as a separate function?
-}
-function getColor()
-function setSize()
-function getSize() {
-  // as the mouse is up from slider sizePicker, it shall reset the grid using that size and default color
-}
 function resetGrid() {
-  // reset the grid to default size, color and mode
+  currentSize = DEFAULT_SIZE;
+  grid.innerText = '';
+  drawGrid(currentSize);
 }
-*/
 
+// Draws a grid of specified size
 function drawGrid(size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -99,10 +82,7 @@ function drawGrid(size) {
   }
 }
 
+// Draws a grid with default values when window loads
 window.onload = () => {
   drawGrid(DEFAULT_SIZE);
-//  console.log(grid.style.gridTemplateColumns);
-  console.log(sizePicker.value);
-  console.log(sizeValue.innerText);
-
 }
